4f71ef4228dc521120ffcde27fbd54bb0d15b138
Jieqi <1400012988@pku.edu.cn>
2017-03-30 12:31:46 +0800

    first commit

---
+++ app/src/main/java/com/example/pku_j/software/MainActivity.java
@@ -0,0 +1,202 @@
+package com.example.pku_j.software;
+
+        import android.content.Intent;
+        import android.graphics.BitmapFactory;
+        import android.net.Uri;
+        import android.widget.AdapterView;
+        import android.widget.Button;
+        import android.graphics.Bitmap;
+        import android.graphics.PorterDuff;
+        import android.graphics.drawable.BitmapDrawable;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.view.MotionEvent;
+        import android.view.View;
+        import android.view.Window;
+        import android.view.WindowManager;
+        import android.widget.EditText;
+        import android.widget.ImageView;
+        import android.content.Context;
+        import android.graphics.Canvas;
+        import android.graphics.Color;
+        import android.graphics.Paint;
+        import android.widget.Spinner;
+        import android.widget.TextView;
+        import android.widget.ListView;
+        import android.widget.PopupWindow;
+        import android.graphics.drawable.ColorDrawable;
+        import android.widget.ArrayAdapter;
+        import android.app.Activity;
+        import android.app.ActivityManager;
+        import android.app.ActivityManager.RunningTaskInfo;
+        import android.content.ComponentName;
+        import android.os.Bundle;
+        import android.os.SystemClock;
+        import android.util.Log;
+        import android.view.Menu;
+
+        import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private float startx, starty, endx, endy;
+    private float centerx, centery;
+    private float radius = 180.0f;
+    private Bitmap baseBitmap;
+    private Canvas canvas;
+    private Paint paint;
+    private ImageView clock;
+
+    public double calcuAng(float x, float y){
+
+        if(x - centerx < 0.001 && x - centerx > -0.001){
+            if (y > centery)
+                return Math.PI/2;
+            else
+                return -Math.PI/2;
+        }
+        return Math.atan2(y-centery, x-centerx);
+    }
+    private Button menu;
+    private ArrayList<String> list = new ArrayList<String>();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getSupportActionBar() != null){
+            getSupportActionBar().hide();
+        }
+        setContentView(R.layout.activity_main);
+
+        clock = (ImageView)findViewById(R.id.clock);
+        list.add("exit");
+        paint = new Paint();
+        paint.setStrokeWidth(1);
+        paint.setColor(Color.RED);
+
+        clock.setOnTouchListener(new PicOnTouchListener());
+
+        menu = (Button)(findViewById(R.id.menu));
+        //spin = (Spinner)(findViewById(R.id.spinner));
+        menu.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                View popupView = MainActivity.this.getLayoutInflater().inflate(R.layout.popupwindow, null);
+
+                ListView lsvMore = (ListView) popupView.findViewById(R.id.lsvMore);
+                lsvMore.setAdapter(new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, list));
+
+                lsvMore.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+                    @Override
+                    public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+                                            long arg3) {
+                        if(list.get(arg2).equals("exit"))System.exit(0);
+                        System.out.print(arg2);
+                    }
+                });
+
+                PopupWindow window = new PopupWindow(popupView, 120, 100);
+
+                window.setFocusable(true);
+                window.setOutsideTouchable(true);
+                window.update();
+                window.showAsDropDown(menu, 0, 20);
+
+                return true;
+            }
+        });
+    }
+
+
+
+
+
+
+    public int calcuTime(double angle){
+        angle = -angle;
+        if (angle > -0.001 && angle < 90)
+            return (int) ((90 - angle)/90 * 15);
+        if (angle > 89.999 && angle < 180.1)
+            return (int) ((180 - angle) / 90 * 15 + 45);
+        if (angle < 0.001 && angle > -90.001)
+            return (int) ((-angle) / 90 * 15 + 15);
+        return (int) ((-angle - 90)/90 * 15 + 30);
+    }
+    private class PicOnTouchListener implements View.OnTouchListener {
+        float startX, startY;
+        Bitmap q;
+        double angle;
+        @Override
+        public boolean onTouch(View v, MotionEvent e) {
+            switch (e.getAction()) {
+                // 用户按下动作
+                case MotionEvent.ACTION_DOWN:
+                    //baseBitmap = Bitmap.createBitmap(clock.getWidth(), clock.getHeight(), Bitmap.Config.ARGB_8888);
+
+                    if(baseBitmap == null) {
+                        clock.setDrawingCacheEnabled(true);
+                        baseBitmap = Bitmap.createBitmap(clock.getDrawingCache());
+                        q = Bitmap.createBitmap(clock.getDrawingCache());
+                        clock.setDrawingCacheEnabled(false);
+                        canvas = new Canvas(baseBitmap);
+                        canvas.drawColor(Color.TRANSPARENT);
+                    }
+                    else {
+                        canvas.drawColor(Color.TRANSPARENT, PorterDuff.Mode.CLEAR);
+                        canvas.drawBitmap(q,0,0,paint);
+                        clock.setImageBitmap(baseBitmap);
+                        clock.invalidate();
+                    }
+                    centerx = clock.getWidth() / 2;
+                    centery =  clock.getHeight() / 2;
+                    startx = e.getX();
+                    starty = e.getY();
+
+                case MotionEvent.ACTION_MOVE:
+                    canvas.drawColor(Color.TRANSPARENT, PorterDuff.Mode.CLEAR);
+                    canvas.drawBitmap(q,0,0,paint);
+                    startx = e.getX();
+                    starty = e.getY();
+
+                    angle = calcuAng(startx, starty);
+                    //System.out.print(startx + " " + starty + "\n");
+                    //System.out.print(centerx + " " + centery + "\n");
+                    //System.out.print("****************");
+                    startx = (int) (centerx + radius * Math.cos(angle));
+                    starty = (int) (centery + radius * Math.sin(angle));
+                    canvas.drawCircle(startx, starty, 10.0f, paint);
+                    clock.setImageBitmap(baseBitmap);
+                    clock.invalidate();
+
+                    ((TextView)(findViewById(R.id.Time))).setText("00:" + String.format("%02d", calcuTime(Math.toDegrees(angle))));
+
+                    break;
+                case MotionEvent.ACTION_UP:
+
+                    int time = calcuTime(Math.toDegrees(angle));
+                    passAng(time);
+                    break;
+                default:
+                    break;
+            }
+
+            return true;
+        }
+
+        public void passAng(int time){
+            ImageView dis=(ImageView)(findViewById(R.id.display));
+            Bitmap bmp= BitmapFactory.decodeResource(getResources(), R.drawable.abc);
+            dis.setImageBitmap(bmp);
+            dis.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View v, MotionEvent event) {
+                    Uri uri = Uri.parse("http://www.baidu.com");
+                    Intent it = new Intent(Intent.ACTION_VIEW, uri);
+                    startActivity(it);
+                    return true;
+                }
+            });
+        }
+    }
+}
---
+++ app/src/main/java/com/example/pku_j/software/Service.java
@@ -0,0 +1,8 @@
+package com.example.pku_j.software;
+
+/**
+ * Created by pku_j on 2017/3/30.
+ */
+
+public class Service  {
+}
